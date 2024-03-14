91c2542e430cf86659482425f2f4c349ad29cea6
Jefferson Duarte <jeffersonsduarte@live.com>
2024-03-14 13:09:11 +0000

    Updated the locker images

--- app/src/main/java/com/stu71205/lab1_71205/gSignUp.kt
+++ app/src/main/java/com/stu71205/lab1_71205/gSignUp.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -49,10 +50,11 @@ fun gSignUp() {
 @Composable
 fun gSignUp_ImgLocker(){
     Image(
-        painter = painterResource(id = R.drawable.locker),
+        painter = painterResource(id = R.drawable.locker_removebg),
         contentDescription = "Locker Imagem",
         modifier = Modifier
             .padding(top = 75.dp, bottom = 25.dp)
+            .requiredSize(110.dp)
     )
 }
 
